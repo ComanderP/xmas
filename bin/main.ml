47ce3ca022f52a6b7f8b65bafdb3270e814903be
ComanderP <8596680+ComanderP@users.noreply.github.com>
2024-02-12 19:40:03 +0100

    Parser errors alpha

--- bin/main.ml
+++ bin/main.ml
@@ -5,7 +5,20 @@ let parse (s : string) : Ast.t =
   let ast = Parser.program Lexer.read lexbuf in
   ast
 
+(* let () =
+   let text = In_channel.input_all stdin in
+   let res = parse text in
+   print_endline (Ast.show res) *)
+
 let () =
   let text = In_channel.input_all stdin in
-  let res = parse text in
-  print_endline (Ast.show res)
+  let res = Driver.fast_parse text in
+  let ast =
+    match res with
+    | Ok ast -> ast
+    | Error err -> (
+        match err with
+        | Lexer.LexicalError msg -> failwith msg
+        | _ -> Driver.parse text)
+  in
+  print_endline (Ast.show ast)
